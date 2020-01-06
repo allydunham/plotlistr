1e877e6ab2dc4d0a614bb24ebc0109a05fe82207
allydunham <ally@ebi.ac.uk>
2020-01-06 11:34:03 +0000

    Added support for saving multipanelfigure objects

--- R/smart_save.R
+++ R/smart_save.R
@@ -36,7 +36,7 @@ smart_save.default <- function(p, filename, ...){
 
   # Remove unecessary params
   params$filename <- NULL
-  params[!names(params) %in% names(formals(ggsave))] <- NULL
+  params[!names(params) %in% names(formals(ggplot2::ggsave))] <- NULL
 
   do.call(ggplot2::ggsave, c(list(filename=filename, plot=p), params))
 }
@@ -56,4 +56,21 @@ smart_save.labeled_plot <- function(p, filename=NULL, override=FALSE, ...){
   params$filename <- NULL
   class(p) <- class(p)[!class(p) == "labeled_plot"]
   do.call(smart_save, c(list(p=p, filename=filename), params))
+}
+
+#' @describeIn smart_save Save \code{multipanelfigure} objects
+#' @export
+smart_save.multipanelfigure <- function(p, filename, ...){
+  if (!requireNamespace("multipanelfigure", quietly = TRUE)) {
+    stop("Attempted to save a \"multipanelfigure\" object without the \"multipanelfigure\" package installed. Please install it.",
+         call. = FALSE)
+  }
+
+  params <- list(...)
+
+  # Remove unecessary params
+  params$filename <- NULL
+  params[!names(params) %in% names(formals(ggplot2::ggsave))] <- NULL
+
+  do.call(multipanelfigure::save_multi_panel_figure, c(list(figure=p, filename=filename), params))
 }
