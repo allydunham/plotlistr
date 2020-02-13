dc6c9264937ec241fd9e4c3eb735f2f1623948e1
allydunham <ally@ebi.ac.uk>
2020-02-13 10:48:11 +0000

    labeled_plot now works on labeled_plot objects
    labeled_plot will now add additional params to labeled_plot objects, overwriting any present that have been given a new value

--- R/labeled_plot.R
+++ R/labeled_plot.R
@@ -3,30 +3,60 @@
 #' Attach labels to a plot object
 #'
 #' \code{labeled_plot} creates a plot object with attributes describing how to
-#' save it
+#' save it. If \code{p} is already a \code{labeled_plot} the additional labels
+#' will be added to it.
 #'
 #' This is an S3 class interacting with the \code{smart_save} S3 generic.
 #'
 #' @param p Plot to wrap
 #' @param overwrite Overwrite existing files by default.
 #' @param filename Name of file to save to, including extension. Will be added
-#'     to the path when saving plot lists. Overides \code{file_format}
+#'     to the path when saving plot lists. Overrides \code{file_format}
 #' @param file_format Format of file to save (e.g. pdf, jpg)
 #' @param units,height,width Size of plot, in \code{units}
 #' @param ... Other parameters passed to saving method (e.g.
 #'     see \code{\link[ggplot2]{ggsave}})
 #'
 #' @export
-labeled_plot <- function(p, overwrite = FALSE, ...){
+labeled_plot <- function(p, ...) {
+  UseMethod("labeled_plot", p)
+}
+
+#' @describeIn labeled_plot Labels to an unlabled plot
+#' @export
+labeled_plot.default <- function(p, ...) {
   class(p) <- c("labeled_plot", class(p))
 
-  params <- list(overwrite = overwrite, ...)
+  params <- list(...)
+
+  # Set overwrite to false by default
+  if (!"overwrite" %in% names(params)) {
+    params$overwrite <- FALSE
+  }
 
   # Remove leading . in file format
-  if ("file_format" %in% names(params)){
+  if ("file_format" %in% names(params)) {
     params$file_format <- gsub("^\\.", "", params$file_format)
   }
 
   attributes(p)$plotlistr_params <- params
+  return(p)
+}
+
+#' @describeIn labeled_plot Add additional labels to a labeled plot
+#' @export
+labeled_plot.labeled_plot <- function(p, ...) {
+  params <- list(...)
+
+  # Remove leading . in file format
+  if ("file_format" %in% names(params)) {
+    params$file_format <- gsub("^\\.", "", params$file_format)
+  }
+
+  # Only keep onld params that aren't being updated
+  old_params <- attributes(p)$plotlistr_params
+  old_params <- old_params[!names(old_params) %in% names(params)]
+
+  attributes(p)$plotlistr_params <- c(params, old_params)
   return(p)
 }
